10bf624863fc42a39286a75e84f3d28c11ebc1de
emirbeyas <emirbeyaz17@outlook.com>
2022-10-31 23:42:49 +0300

    finalfinaledit

--- app.py
+++ app.py
@@ -10,22 +10,22 @@ def home():
     forward_message = ""
     return render_template('index.html', content = dbContent, forward_message=forward_message)
 
-@app.route("/sendMessage", methods=['POST'])
-def sendMessage():
-    if request.method == "POST":
-        senderName = request.form.get("senderName")
-        senderMail = request.form.get("senderMail")
-        senderSubject = request.form.get("senderSubject")
-        senderMessage = request.form.get("senderMessage")
+# @app.route("/sendMessage", methods=['POST'])
+# def sendMessage():
+#     if request.method == "POST":
+#         senderName = request.form.get("senderName")
+#         senderMail = request.form.get("senderMail")
+#         senderSubject = request.form.get("senderSubject")
+#         senderMessage = request.form.get("senderMessage")
 
-        dbContent = db.webContent()
-        dbContent.sendMessage(senderName, senderMail, senderSubject, senderMessage)
-        forward_message = "message has been sent..."
-        return render_template('index.html', content = dbContent, forward_message=forward_message)
-    else:
-        dbContent = db.webContent()
-        forward_message = "something went wrong..."
-        return render_template('index.html', content = dbContent, forward_message=forward_message)
+#         dbContent = db.webContent()
+#         dbContent.sendMessage(senderName, senderMail, senderSubject, senderMessage)
+#         forward_message = "message has been sent..."
+#         return render_template('index.html', content = dbContent, forward_message=forward_message)
+#     else:
+#         dbContent = db.webContent()
+#         forward_message = "something went wrong..."
+#         return render_template('index.html', content = dbContent, forward_message=forward_message)
 
 if __name__ == "__main__":
     app.run()
